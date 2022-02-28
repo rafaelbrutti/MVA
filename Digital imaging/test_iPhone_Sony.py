2e1f75810ad7a1ca85555b4ecfc2cd1b2f8fa713
rafaelbrutti <79722133+rafaelbrutti@users.noreply.github.com>
2022-02-28 11:47:49 +0100

    Add code

---
+++ Digital imaging/test_iPhone_Sony.py
@@ -0,0 +1,342 @@
+# uniform content loss + adaptive threshold + per_class_input + recursive G
+# improvement upon cqf37
+from __future__ import division
+import os, scipy.io
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import numpy as np
+import rawpy
+import glob
+
+input_dir = './dataset/Test/short/'
+gt_dir = './dataset/Test/long/'
+checkpoint_dir = './checkpoint/Sony/'
+result_dir = './result_test_withSony/'
+
+# get test IDs
+test_fns = glob.glob(gt_dir + '1*.ARW')
+test_ids = [int(os.path.basename(test_fn)[0:5]) for test_fn in test_fns]
+
+DEBUG = 0
+if DEBUG == 1:
+    save_freq = 2
+    test_ids = test_ids[0:5]
+
+
+def lrelu(x):
+    return tf.maximum(x * 0.2, x)
+
+
+def upsample_and_concat(x1, x2, output_channels, in_channels):
+    pool_size = 2
+    deconv_filter = tf.Variable(tf.truncated_normal([pool_size, pool_size, output_channels, in_channels], stddev=0.02))
+    deconv = tf.nn.conv2d_transpose(x1, deconv_filter, tf.shape(x2), strides=[1, pool_size, pool_size, 1])
+
+    deconv_output = tf.concat([deconv, x2], 3)
+    deconv_output.set_shape([None, None, None, output_channels * 2])
+
+    return deconv_output
+
+
+def network(input):
+    conv1 = slim.conv2d(input, 32, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv1_1')
+    conv1 = slim.conv2d(conv1, 32, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv1_2')
+    pool1 = slim.max_pool2d(conv1, [2, 2], padding='SAME')
+
+    conv2 = slim.conv2d(pool1, 64, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv2_1')
+    conv2 = slim.conv2d(conv2, 64, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv2_2')
+    pool2 = slim.max_pool2d(conv2, [2, 2], padding='SAME')
+
+    conv3 = slim.conv2d(pool2, 128, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv3_1')
+    conv3 = slim.conv2d(conv3, 128, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv3_2')
+    pool3 = slim.max_pool2d(conv3, [2, 2], padding='SAME')
+
+    conv4 = slim.conv2d(pool3, 256, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv4_1')
+    conv4 = slim.conv2d(conv4, 256, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv4_2')
+    pool4 = slim.max_pool2d(conv4, [2, 2], padding='SAME')
+
+    conv5 = slim.conv2d(pool4, 512, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv5_1')
+    conv5 = slim.conv2d(conv5, 512, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv5_2')
+
+    up6 = upsample_and_concat(conv5, conv4, 256, 512)
+    conv6 = slim.conv2d(up6, 256, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv6_1')
+    conv6 = slim.conv2d(conv6, 256, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv6_2')
+
+    up7 = upsample_and_concat(conv6, conv3, 128, 256)
+    conv7 = slim.conv2d(up7, 128, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv7_1')
+    conv7 = slim.conv2d(conv7, 128, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv7_2')
+
+    up8 = upsample_and_concat(conv7, conv2, 64, 128)
+    conv8 = slim.conv2d(up8, 64, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv8_1')
+    conv8 = slim.conv2d(conv8, 64, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv8_2')
+
+    up9 = upsample_and_concat(conv8, conv1, 32, 64)
+    conv9 = slim.conv2d(up9, 32, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv9_1')
+    conv9 = slim.conv2d(conv9, 32, [3, 3], rate=1, activation_fn=lrelu, scope='g_conv9_2')
+
+    conv10 = slim.conv2d(conv9, 12, [1, 1], rate=1, activation_fn=None, scope='g_conv10')
+    out = tf.depth_to_space(conv10, 2)
+    return out
+
+
+def pack_raw(raw):
+    # pack Bayer image to 4 channels
+    im = raw.raw_image_visible.astype(np.float32)
+    im = np.maximum(im - 512, 0) / (16383 - 512)  # subtract the black level
+
+    im = np.expand_dims(im, axis=2)
+    img_shape = im.shape
+    H = img_shape[0]
+    W = img_shape[1]
+
+    out = np.concatenate((im[0:H:2, 0:W:2, :],
+                          im[0:H:2, 1:W:2, :],
+                          im[1:H:2, 1:W:2, :],
+                          im[1:H:2, 0:W:2, :]), axis=2)
+    return out
+
+
+sess = tf.Session()
+in_image = tf.placeholder(tf.float32, [None, None, None, 4])
+gt_image = tf.placeholder(tf.float32, [None, None, None, 3])
+out_image = network(in_image)
+
+saver = tf.train.Saver()
+sess.run(tf.global_variables_initializer())
+ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+if ckpt:
+    print('loaded ' + ckpt.model_checkpoint_path)
+    saver.restore(sess, ckpt.model_checkpoint_path)
+
+if not os.path.isdir(result_dir + 'final/'):
+    os.makedirs(result_dir + 'final/')
+
+####################### OLD SCIPY.MISC
+
+from PIL import Image
+
+def bytescale(data, cmin=None, cmax=None, high=255, low=0):
+    """
+    Byte scales an array (image).
+    Byte scaling means converting the input image to uint8 dtype and scaling
+    the range to ``(low, high)`` (default 0-255).
+    If the input image already has dtype uint8, no scaling is done.
+    This function is only available if Python Imaging Library (PIL) is installed.
+    Parameters
+    ----------
+    data : ndarray
+        PIL image data array.
+    cmin : scalar, optional
+        Bias scaling of small values. Default is ``data.min()``.
+    cmax : scalar, optional
+        Bias scaling of large values. Default is ``data.max()``.
+    high : scalar, optional
+        Scale max value to `high`.  Default is 255.
+    low : scalar, optional
+        Scale min value to `low`.  Default is 0.
+    Returns
+    -------
+    img_array : uint8 ndarray
+        The byte-scaled array.
+    Examples
+    --------
+    >>> from scipy.misc import bytescale
+    >>> img = np.array([[ 91.06794177,   3.39058326,  84.4221549 ],
+    ...                 [ 73.88003259,  80.91433048,   4.88878881],
+    ...                 [ 51.53875334,  34.45808177,  27.5873488 ]])
+    >>> bytescale(img)
+    array([[255,   0, 236],
+           [205, 225,   4],
+           [140,  90,  70]], dtype=uint8)
+    >>> bytescale(img, high=200, low=100)
+    array([[200, 100, 192],
+           [180, 188, 102],
+           [155, 135, 128]], dtype=uint8)
+    >>> bytescale(img, cmin=0, cmax=255)
+    array([[91,  3, 84],
+           [74, 81,  5],
+           [52, 34, 28]], dtype=uint8)
+    """
+    if data.dtype == np.uint8:
+        return data
+
+    if high > 255:
+        raise ValueError("`high` should be less than or equal to 255.")
+    if low < 0:
+        raise ValueError("`low` should be greater than or equal to 0.")
+    if high < low:
+        raise ValueError("`high` should be greater than or equal to `low`.")
+
+    if cmin is None:
+        cmin = data.min()
+    if cmax is None:
+        cmax = data.max()
+
+    cscale = cmax - cmin
+    if cscale < 0:
+        raise ValueError("`cmax` should be larger than `cmin`.")
+    elif cscale == 0:
+        cscale = 1
+
+    scale = float(high - low) / cscale
+    bytedata = (data - cmin) * scale + low
+    return (bytedata.clip(low, high) + 0.5).astype(np.uint8)
+
+
+def toimage(arr, high=255, low=0, cmin=None, cmax=None, pal=None,
+            mode=None, channel_axis=None):
+    """Takes a numpy array and returns a PIL image.
+    This function is only available if Python Imaging Library (PIL) is installed.
+    The mode of the PIL image depends on the array shape and the `pal` and
+    `mode` keywords.
+    For 2-D arrays, if `pal` is a valid (N,3) byte-array giving the RGB values
+    (from 0 to 255) then ``mode='P'``, otherwise ``mode='L'``, unless mode
+    is given as 'F' or 'I' in which case a float and/or integer array is made.
+    .. warning::
+        This function uses `bytescale` under the hood to rescale images to use
+        the full (0, 255) range if ``mode`` is one of ``None, 'L', 'P', 'l'``.
+        It will also cast data for 2-D images to ``uint32`` for ``mode=None``
+        (which is the default).
+    Notes
+    -----
+    For 3-D arrays, the `channel_axis` argument tells which dimension of the
+    array holds the channel data.
+    For 3-D arrays if one of the dimensions is 3, the mode is 'RGB'
+    by default or 'YCbCr' if selected.
+    The numpy array must be either 2 dimensional or 3 dimensional.
+    """
+    data = np.asarray(arr)
+    if np.iscomplexobj(data):
+        raise ValueError("Cannot convert a complex-valued array.")
+    shape = list(data.shape)
+    valid = len(shape) == 2 or ((len(shape) == 3) and
+                                ((3 in shape) or (4 in shape)))
+    if not valid:
+        raise ValueError("'arr' does not have a suitable array shape for "
+                         "any mode.")
+    if len(shape) == 2:
+        shape = (shape[1], shape[0])  # columns show up first
+        if mode == 'F':
+            data32 = data.astype(np.float32)
+            image = Image.frombytes(mode, shape, data32.tostring())
+            return image
+        if mode in [None, 'L', 'P']:
+            bytedata = bytescale(data, high=high, low=low,
+                                 cmin=cmin, cmax=cmax)
+            image = Image.frombytes('L', shape, bytedata.tostring())
+            if pal is not None:
+                image.putpalette(np.asarray(pal, dtype=np.uint8).tostring())
+                # Becomes a mode='P' automagically.
+            elif mode == 'P':  # default gray-scale
+                pal = (np.arange(0, 256, 1, dtype=np.uint8)[:, np.newaxis] *
+                       np.ones((3,), dtype=np.uint8)[np.newaxis, :])
+                image.putpalette(np.asarray(pal, dtype=np.uint8).tostring())
+            return image
+        if mode == '1':  # high input gives threshold for 1
+            bytedata = (data > high)
+            image = Image.frombytes('1', shape, bytedata.tostring())
+            return image
+        if cmin is None:
+            cmin = np.amin(np.ravel(data))
+        if cmax is None:
+            cmax = np.amax(np.ravel(data))
+        data = (data*1.0 - cmin)*(high - low)/(cmax - cmin) + low
+        if mode == 'I':
+            data32 = data.astype(np.uint32)
+            image = Image.frombytes(mode, shape, data32.tostring())
+        else:
+            raise ValueError(_errstr)
+        return image
+
+    # if here then 3-d array with a 3 or a 4 in the shape length.
+    # Check for 3 in datacube shape --- 'RGB' or 'YCbCr'
+    if channel_axis is None:
+        if (3 in shape):
+            ca = np.flatnonzero(np.asarray(shape) == 3)[0]
+        else:
+            ca = np.flatnonzero(np.asarray(shape) == 4)
+            if len(ca):
+                ca = ca[0]
+            else:
+                raise ValueError("Could not find channel dimension.")
+    else:
+        ca = channel_axis
+
+    numch = shape[ca]
+    if numch not in [3, 4]:
+        raise ValueError("Channel axis dimension is not valid.")
+
+    bytedata = bytescale(data, high=high, low=low, cmin=cmin, cmax=cmax)
+    if ca == 2:
+        strdata = bytedata.tostring()
+        shape = (shape[1], shape[0])
+    elif ca == 1:
+        strdata = np.transpose(bytedata, (0, 2, 1)).tostring()
+        shape = (shape[2], shape[0])
+    elif ca == 0:
+        strdata = np.transpose(bytedata, (1, 2, 0)).tostring()
+        shape = (shape[2], shape[1])
+    if mode is None:
+        if numch == 3:
+            mode = 'RGB'
+        else:
+            mode = 'RGBA'
+
+    if mode not in ['RGB', 'RGBA', 'YCbCr', 'CMYK']:
+        raise ValueError(_errstr)
+
+    if mode in ['RGB', 'YCbCr']:
+        if numch != 3:
+            raise ValueError("Invalid array shape for mode.")
+    if mode in ['RGBA', 'CMYK']:
+        if numch != 4:
+            raise ValueError("Invalid array shape for mode.")
+
+    # Here we know data and mode is correct
+    image = Image.frombytes(mode, shape, strdata)
+    return image
+
+for test_id in test_ids:
+    # test the first image in each sequence
+    in_files = glob.glob(input_dir + '%05d_00*.ARW' % test_id)
+    for k in range(len(in_files)):
+        in_path = in_files[k]
+        in_fn = os.path.basename(in_path)
+        print(in_fn)
+        gt_files = glob.glob(gt_dir + '%05d_00*.ARW' % test_id)
+        gt_path = gt_files[0]
+        gt_fn = os.path.basename(gt_path)
+        in_exposure = float(in_fn[9:-5])
+        gt_exposure = float(gt_fn[9:-5])
+        #ratio = min(gt_exposure / in_exposure, 300)
+        ratio  = 100
+        raw = rawpy.imread(in_path)
+        input_full = np.expand_dims(pack_raw(raw), axis=0) * ratio
+
+        im = raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+        # scale_full = np.expand_dims(np.float32(im/65535.0),axis = 0)*ratio
+        scale_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
+
+        gt_raw = rawpy.imread(gt_path)
+        im = gt_raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
+        gt_full = np.expand_dims(np.float32(im / 65535.0), axis=0)
+
+        input_full = np.minimum(input_full, 1.0)
+
+        output = sess.run(out_image, feed_dict={in_image: input_full})
+        output = np.minimum(np.maximum(output, 0), 1)
+
+        output = output[0, :, :, :]
+        gt_full = gt_full[0, :, :, :]
+        scale_full = scale_full[0, :, :, :]
+        scale_full = scale_full * np.mean(gt_full) / np.mean(
+            scale_full)  # scale the low-light image to the same mean of the groundtruth
+
+        toimage(output * 255, high=255, low=0, cmin=0, cmax=255).save(
+                result_dir + 'final/%5d_00_%d_out.png' % (test_id, ratio))
+        toimage(scale_full * 255, high=255, low=0, cmin=0, cmax=255).save(
+                result_dir + 'final/%5d_00_%d_scale.png' % (test_id, ratio))
+        toimage(gt_full * 255, high=255, low=0, cmin=0, cmax=255).save(
+                result_dir + 'final/%5d_00_%d_gt.png' % (test_id, ratio))
+
+
